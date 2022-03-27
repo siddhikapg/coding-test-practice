c839dc18a7e6e98bf1ae2bfcfdbe5d35ed035d2b
Siddhika Ghaisas <siddhikapg@gmail.com>
2022-03-27 15:52:53 -0500

    added changes to palindrome program for special characters in the string

--- src/Palindrome.py
+++ src/Palindrome.py
@@ -1,14 +1,20 @@
 
 def main():
     print("Hello world")
-    ans = isPalindrome("abcba")
+    ans = isPalindrome("ada")
     print(ans)
 def isPalindrome(sequence):
     start = 0
     end = len(sequence)-1
     pali = True
     while pali:
-        if sequence[start] is sequence[end]:
+        if not sequence[start].isalnum():
+            start = start + 1
+            continue
+        if not sequence[end].isalnum():
+            end = end - 1
+            continue
+        if sequence[start].lower() == sequence[end].lower():
             if start == end:
                 break
             start = start + 1
